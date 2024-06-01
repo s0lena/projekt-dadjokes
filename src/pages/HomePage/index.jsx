2125b2182c80d5c15cfdb105983280de436d378b
s0lena <solena.work@gmail.com>
2024-06-01 13:00:34 +0300

    Brought the project in line with the sample according to the task

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,23 +1,34 @@
+import { useState } from 'react';
 import './style.css';
 
 export const HomePage = () => {
+  const [likes, setLikes]=useState(0);
+  const [dislikes, setDislikes]=useState(0);
+
+
   return (
     <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          {" "}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img className="user-avatar" src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png" />
+            <p className="user-name">Neroxx</p>
+          </div>
+
+          <p className="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+        <div className="joke__likes">
+          <button id="btn-up" className="btn-like btn-like--up" onClick={()=>setLikes(likes+1)}></button>
+          <span id="likes-up" className="likes-count likes-count--up">{likes}</span>
+          <button id="btn-down" className="btn-like btn-like--down" onClick={()=>setDislikes(dislikes+1)}></button>
+          <span id="likes-down" className="likes-count likes-count--down">{dislikes}</span>
+        </div>
+      </div>
     </div>
+
   );
 };
